53fe333534f3fad661de16968aaaa9c30992198f
Miguel Castro <ma.castro9714@gmail.com>
2019-05-25 12:52:03 -0500

    Minor Structure Adjustments

--- restaurants/src/components/Carousel.js
+++ restaurants/src/components/Carousel.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-class Carrousel extends React.Component {
+class Carousel extends React.Component {
   render() {
     return <div>Carrousel</div>;
   }
 }
 
-export default Carrousel;
+export default Carousel;
