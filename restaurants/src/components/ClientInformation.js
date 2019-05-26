23c2fb7c6778df6a391fbf574381c54303f197da
Arka Ariza <arjarizami@unal.edu.co>
2019-05-25 21:05:42 -0500

    FIXED

--- restaurants/src/components/ClientInformation.js
+++ restaurants/src/components/ClientInformation.js
@@ -1,8 +1,53 @@
 import React from "react";
+import $ from "jquery";
+import accordion from "jquery-ui";
 
 class ClientInformation extends React.Component {
   render() {
-    return <div className="ui centered card">asdad</div>;
+    return (
+      <div
+        className="ui left aligned centered card"
+        style={{ textAlign: "left" }}>
+        <div className="content">
+          <h1>Reservation</h1>
+        </div>
+        <div className="ui styled accordion">
+          <div className="title">
+            <i className="dropdown icon" />
+            Breakfast
+          </div>
+          <div className="content">
+            <h4>Restaurant Name</h4>
+            <h4>Location</h4>
+            <h4>Hour</h4>
+          </div>
+        </div>
+
+        <div className="ui styled accordion">
+          <div className="title">
+            <i className="dropdown icon" />
+            Lunch
+          </div>
+          <div className="content">
+            <h4>Restaurant Name</h4>
+            <h4>Location</h4>
+            <h4>Hour</h4>
+          </div>
+        </div>
+
+        <div className="ui styled accordion">
+          <div className="title">
+            <i className="dropdown icon" />
+            Diner
+          </div>
+          <div className="content">
+            <h4>Restaurant Name</h4>
+            <h4>Location</h4>
+            <h4>Hour</h4>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
