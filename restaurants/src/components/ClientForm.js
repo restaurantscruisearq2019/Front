308ca174b52cdf22e216afb6e303852acf176426
Arka Ariza <arjarizami@unal.edu.co>
2019-06-02 22:41:05 -0500

    Flexing

--- restaurants/src/components/ClientForm.js
+++ restaurants/src/components/ClientForm.js
@@ -23,33 +23,31 @@ class ClientForm extends React.Component {
 
   render() {
     return (
-      <div className="ui middle aligned center aligned grid container">
-        <div className="six wide column">
-          <h2 className="ui image header">
-            <div className="content">Check your reservation!</div>
-          </h2>
-          <form className="ui large form">
-            <div className="ui stacked segment">
-              <div className="field">
-                <div className="ui left icon input">
-                  <i className="user icon" />
-                  <input
-                    type="text"
-                    placeholder="Client Id"
-                    onChange={this.handleChange}
-                    value={this.state.id}
-                  />
-                </div>
+      <div>
+        <h2 className="ui centered header">
+          <div className="content">Check your reservation!</div>
+        </h2>
+        <form className="ui large form">
+          <div className="ui stacked segment">
+            <div className="field">
+              <div className="ui left icon input">
+                <i className="user icon" />
+                <input
+                  type="text"
+                  placeholder="Client Id"
+                  onChange={this.handleChange}
+                  value={this.state.id}
+                />
               </div>
-              <button
-                className="ui fluid secondary large submit button"
-                onClick={this.handleSubmit}>
-                Submit
-              </button>
             </div>
-            <div className="ui error message" />
-          </form>
-        </div>
+            <button
+              className="ui fluid secondary large submit button"
+              onClick={this.handleSubmit}>
+              Submit
+            </button>
+          </div>
+          <div className="ui error message" />
+        </form>
       </div>
     );
   }
