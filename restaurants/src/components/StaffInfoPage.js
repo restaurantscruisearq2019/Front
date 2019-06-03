9ed2287c5983128cd9c524b2709b29825e3fa913
miacastroco <miacastroco@unal.edu.co>
2019-06-03 14:33:09 -0500

    Manager Login, Employees Info

--- restaurants/src/components/StaffInfoPage.js
+++ restaurants/src/components/StaffInfoPage.js
@@ -1,38 +1,20 @@
 import React from "react";
 
 class StaffInfoPage extends React.Component {
-  state = {
-    employees: [
-      { name: "asdasd", office: "asdsad" },
-      { name: "asdasd", office: "asdsad" },
-      { name: "asdasd", office: "asdsad" },
-      { name: "asdasd", office: "asdsad" },
-      { name: "asdasd", office: "asdsad" },
-      { name: "asdasd", office: "asdsad" },
-      { name: "asdasd", office: "asdsad" },
-      { name: "asdasd", office: "asdsad" },
-      { name: "asdasd", office: "asdsad" },
-      { name: "asdasd", office: "asdsad" }
-    ]
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      employees: props.userInfo.employees
+    };
+  }
 
   renderList() {
     return this.state.employees.map(employee => {
       return (
-        <div className="item">
-          <div className="right floated content">
-            <div className="ui dropdown">
-              <i className="bars icon" />
-              <div className="right menu">
-                <div className="item">{employee.name}</div>
-                <div className="item">{employee.office}</div>
-                <div className="item">Delete</div>
-              </div>
-            </div>
-          </div>
-          <img alt="" className="ui avatar image" src="default" />
+        <div key={employee._id} className="ui fluid blue card">
           <div className="content">
-            <div className="header">{employee.name}</div>
+              <div className="center aligned header">{employee.name}</div>
+              <div className="center aligned">{employee.office}</div>
           </div>
         </div>
       );
@@ -41,7 +23,7 @@ class StaffInfoPage extends React.Component {
 
   render() {
     return (
-      <div className="ui middle aligned massive list">{this.renderList()}</div>
+      <div style={{alignSelf:"center"}} className="ui middle aligned centered massive list container">{this.renderList()}</div>
     );
   }
 }
