445ddcf609a57ce30ac5762bd6e7f3b092e6d1a6
miacastroco <miacastroco@unal.edu.co>
2019-06-03 00:36:43 -0500

    Price Range

--- restaurants/src/components/Prices.js
+++ restaurants/src/components/Prices.js
@@ -1,46 +1,71 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import gql from "graphql-tag";
+import apollo from "../api/apollo";
+import CategoryCard from './CategoryCard';
 
 class Prices extends React.Component {
+
+  state = {
+    priceRange: null, 
+    priceRangeRestaurants: null
+  }
+
+  fetchRestaurants = ( a ) => {
+    apollo
+      .query({
+        query: gql`
+          {
+            getRestaurantsByPricerange(priceRange: "${a}"){
+              name
+              _id
+            }
+          }
+        `
+      })
+      .then(data => {
+        this.setState({ priceRangeRestaurants: data.data.getRestaurantsByPricerange });
+        this.setState({ priceRange: a });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  renderRestaurants(){
+    if (!this.state.priceRangeRestaurants) {
+      return <h2>Select a Price Range!</h2>;
+    }
+    return this.state.priceRangeRestaurants.map( r => {
+      return (
+        <CategoryCard key={r._id} name={r.name} category={this.state.priceRange}/>
+      )
+    });
+  }
+
   render() {
     return (
       <div
         id="reservationgrid"
         className="ui sixteen wide column two column relaxed stackable grid middle aligned content container">
-        <div className="ui middle aligned grid">
           <div className="four wide column">
             <div className="ui huge header">Restaurant prices</div>
-            <div className="ui vertical fluid tabular menu">
-              <Link to="undefined" className="active item">
-                BurguerHit
-              </Link>
-              <Link to="undefined" className="item">
-                BurguerMegaHit
-              </Link>
-              <Link to="undefined" className="item">
-                BurguerHyperHit
-              </Link>
-              <Link to="undefined" className="item">
-                VegieHit
-              </Link>
-              <Link to="undefined" className="item">
-                VegieMegaHit
-              </Link>
-              <Link to="undefined" className="item">
-                VegieHyperHit
-              </Link>
+            <div className="ui secondary vertical pointing menu">
+              <button onClick={() => this.fetchRestaurants("10-20")} className="item">
+                10-20 (Dollars)
+              </button>
+              <button onClick={() => this.fetchRestaurants("20-30")} className="item">
+                20-30 (Dollars)
+              </button>
+              <button onClick={() => this.fetchRestaurants("30-40")} className="item">
+                30-40 (Dollars)
+              </button>
             </div>
           </div>
-          <div className="twelve wide stretched column">
-            <div className="ui segment">
-              <img
-                alt=""
-                className="ui middle aligned rounded fluid image"
-                src="https://pbs.twimg.com/media/B7fRc45IIAELZqP.jpg"
-              />
+          <div className="eight wide column internally celled grid">
+            <div>
+              {this.renderRestaurants()}
             </div>
           </div>
-        </div>
       </div>
     );
   }
