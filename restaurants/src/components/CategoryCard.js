bccf4451bd7ccd86b6c914176fe4c4748666448f
miacastroco <miacastroco@unal.edu.co>
2019-06-03 00:58:07 -0500

    Menu Queries

--- restaurants/src/components/CategoryCard.js
+++ restaurants/src/components/CategoryCard.js
@@ -3,14 +3,14 @@ import React from "react";
 const CategoryCard = ({ name, category}) => {
     return (
         <div className="ui fluid centered red card">
-        <div className="content">
-            <div className="center aligned header">{name}</div>
-        </div>
-        <div className="extra content">
-            <div className="center aligned author">
-            <i className="ui avatar image utensils icon" /> {category}
+            <div className="content">
+                <div className="center aligned header">{name}</div>
             </div>
-        </div>
+            <div className="extra content">
+                <div className="center aligned author">
+                <i className="ui avatar image utensils icon" /> {category}
+                </div>
+            </div>
         </div>
     );
 }
--- restaurants/src/components/ListMenus.js
+++ restaurants/src/components/ListMenus.js
@@ -1,42 +1,78 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import gql from "graphql-tag";
+import apollo from "../api/apollo";
+import CategoryCard from './CategoryCard';
 
 class ListMenus extends React.Component {
+
+  state = {
+    category: null, 
+    categoryMenuRestaurants: null
+  }
+
+  fetchRestaurants = ( a ) => {
+    apollo
+      .query({
+        query: gql`
+        {
+          getRestaurantsByMenu(mType: "${a}"){
+            name
+            _id
+          }
+        }        
+        `
+      })
+      .then(data => {
+        this.setState({ categoryMenuRestaurants: data.data.getRestaurantsByMenu });
+        this.setState({ category: a });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  renderRestaurants(){
+    if (!this.state.categoryMenuRestaurants) {
+      return <h2>Select a Type of Menu!</h2>;
+    }
+    return this.state.categoryMenuRestaurants.map( r => {
+      return (
+        <CategoryCard key={r._id} name={r.name} category={this.state.category}/>
+      )
+    });
+  }
+
   render() {
     return (
       <div
         id="reservationgrid"
         className="ui sixteen wide column two column relaxed stackable grid middle aligned content container">
         <div className="five wide column">
-          <div className="ui huge header">Types of menus</div>
-
           <div className="ui secondary vertical menu">
-            <Link to="undefined" className="active item">
-              tasting
-            </Link>
-            <Link to="undefined" className="item">
-              today's menu
-            </Link>
-            <Link to="undefined" className="active item">
-              executive
-            </Link>
-            <Link to="undefined" className="item">
-              fast foods
-            </Link>
-            <Link to="undefined" className="active item">
-              for children
-            </Link>
-            <Link to="undefined" className="item">
-              menu Link la carte
-            </Link>
+            <button onClick={() => this.fetchRestaurants("Burguers")} className="active item">
+              Burguers
+            </button>
+            <button onClick={() => this.fetchRestaurants("Vegie")} className="active item">
+              Vegie
+            </button>
+            <button onClick={() => this.fetchRestaurants("Meats")} className="active item">
+              Meats
+            </button>
+            <button onClick={() => this.fetchRestaurants("Italian")} className="active item">
+              Italian
+            </button>
+            <button onClick={() => this.fetchRestaurants("Pizza")} className="active item">
+              Pizza
+            </button>
+            <button onClick={() => this.fetchRestaurants("Sushi")} className="active item">
+              Sushi
+            </button>
           </div>
         </div>
-        <div className="eight wide column">
-          <img
-            alt=""
-            className="ui middle aligned rounded fluid image"
-            src="http://www.agendatuxtla.com/img/RESTAURANTES/LA%20MORENA/Cartas%20La%20Morena-01.jpg"
-          />
+        <div className="eight wide column internally celled grid">
+          <div>
+            {this.renderRestaurants()}
+          </div>
         </div>
       </div>
     );
