1e69712aaa4b4a78fe14fd908630eb27825aee53
Arka Ariza <arjarizami@unal.edu.co>
2019-06-16 22:46:29 -0500

    fixed token set

--- restaurants/src/api/apollo.js
+++ restaurants/src/api/apollo.js
@@ -1,12 +1,23 @@
 import ApolloClient from "apollo-boost";
+import { ApolloLink, concat } from "apollo-link";
+import { HttpLink, createHttpLink } from "apollo-link-http";
 
 export default new ApolloClient({
   uri: "http://ec2-3-84-165-200.compute-1.amazonaws.com:5000/graphql",
-  //"uri: http://localhost:5000/graphql",
-  headers: {
-    authorization: localStorage.getItem("jwtToken")
+  request: async operation => {
+    const token = await localStorage.getItem("jwtToken");
+    operation.setContext({
+      headers: {
+        authorization: token
+      }
+    });
   }
 });
+
+/* export default new ApolloClient({
+  uri: "http://ec2-3-84-165-200.compute-1.amazonaws.com:5000/graphql",
+  link: concat(authMiddleware, httpLink)
+}); */
 
 export const apolloAuth = new ApolloClient({
   uri: "http://ec2-3-84-165-200.compute-1.amazonaws.com:5000/auth"
