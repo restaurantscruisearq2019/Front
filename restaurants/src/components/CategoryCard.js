1edf3ac254db231a7eb6654a8f4f3c5005975069
Arka Ariza <arjarizami@unal.edu.co>
2019-06-16 23:42:11 -0500

    image height

--- restaurants/src/components/CategoryCard.js
+++ restaurants/src/components/CategoryCard.js
@@ -20,7 +20,7 @@ const CategoryCard = ({ name, category, description, images }) => {
         <Carousel autoPlay showThumbs={false} infiniteLoop={true}>
           <div>
             <img
-              style={{ width: "100%", height: "400px" }}
+              style={{ width: "100%", height: "200px" }}
               alt=""
               src={images}
             />
