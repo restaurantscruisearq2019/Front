0bdd475068d2411a0ba2b841e6f474fd2ce68f79
Arka Ariza <arjarizami@unal.edu.co>
2019-06-17 00:01:30 -0500

    prices fixed

--- restaurants/src/components/Categories.js
+++ restaurants/src/components/Categories.js
@@ -1,16 +1,15 @@
 import React from "react";
 import gql from "graphql-tag";
 import apollo from "../api/apollo";
-import CategoryCard from './CategoryCard';
+import CategoryCard from "./CategoryCard";
 
 class Categories extends React.Component {
-
   state = {
-    category: null, 
+    category: null,
     categoryRestaurants: null
-  }
+  };
 
-  fetchRestaurants = ( a ) => {
+  fetchRestaurants = a => {
     apollo
       .query({
         query: gql`
@@ -25,22 +24,30 @@ class Categories extends React.Component {
         `
       })
       .then(data => {
-        this.setState({ categoryRestaurants: data.data.getRestaurantsByCategories });
+        this.setState({
+          categoryRestaurants: data.data.getRestaurantsByCategories
+        });
         this.setState({ category: a });
       })
       .catch(err => {
         console.log(err);
       });
-  }
+  };
 
-  renderRestaurants(){
+  renderRestaurants() {
     if (!this.state.categoryRestaurants) {
       return <h2>Select a Category!</h2>;
     }
-    return this.state.categoryRestaurants.map( r => {
+    return this.state.categoryRestaurants.map(r => {
       return (
-        <CategoryCard key={r._id} description={r.description} images= {r.images} name={r.name} category={this.state.category}/>
-      )
+        <CategoryCard
+          key={r._id}
+          description={r.description}
+          images={r.images}
+          name={r.name}
+          category={this.state.category}
+        />
+      );
     });
   }
 
@@ -51,30 +58,40 @@ class Categories extends React.Component {
         className="ui sixteen wide column two column relaxed stackable grid middle aligned content container">
         <div className="five wide column">
           <div className="ui secondary vertical menu">
-            <button onClick={() => this.fetchRestaurants("Burguers")} className="active item">
+            <button
+              onClick={() => this.fetchRestaurants("Burguers")}
+              className="active item">
               Burguers
             </button>
-            <button onClick={() => this.fetchRestaurants("Vegie")} className="active item">
+            <button
+              onClick={() => this.fetchRestaurants("Vegie")}
+              className="active item">
               Vegie
             </button>
-            <button onClick={() => this.fetchRestaurants("Meats")} className="active item">
+            <button
+              onClick={() => this.fetchRestaurants("Meats")}
+              className="active item">
               Meats
             </button>
-            <button onClick={() => this.fetchRestaurants("Italian")} className="active item">
+            <button
+              onClick={() => this.fetchRestaurants("Italian")}
+              className="active item">
               Italian
             </button>
-            <button onClick={() => this.fetchRestaurants("Pizza")} className="active item">
+            <button
+              onClick={() => this.fetchRestaurants("Pizza")}
+              className="active item">
               Pizza
             </button>
-            <button onClick={() => this.fetchRestaurants("Sushi")} className="active item">
+            <button
+              onClick={() => this.fetchRestaurants("Sushi")}
+              className="active item">
               Sushi
             </button>
           </div>
         </div>
         <div className="eight wide column internally celled grid">
-          <div>
-            {this.renderRestaurants()}
-          </div>
+          <div>{this.renderRestaurants()}</div>
         </div>
       </div>
     );
--- restaurants/src/components/Prices.js
+++ restaurants/src/components/Prices.js
@@ -1,16 +1,15 @@
 import React from "react";
 import gql from "graphql-tag";
 import apollo from "../api/apollo";
-import CategoryCard from './CategoryCard';
+import CategoryCard from "./CategoryCard";
 
 class Prices extends React.Component {
-
   state = {
-    priceRange: null, 
+    priceRange: null,
     priceRangeRestaurants: null
-  }
+  };
 
-  fetchRestaurants = ( a ) => {
+  fetchRestaurants = a => {
     apollo
       .query({
         query: gql`
@@ -18,27 +17,37 @@ class Prices extends React.Component {
             getRestaurantsByPricerange(priceRange: "${a}"){
               name
               _id
+              description
+              images
             }
           }
         `
       })
       .then(data => {
-        this.setState({ priceRangeRestaurants: data.data.getRestaurantsByPricerange });
+        this.setState({
+          priceRangeRestaurants: data.data.getRestaurantsByPricerange
+        });
         this.setState({ priceRange: a });
       })
       .catch(err => {
         console.log(err);
       });
-  }
+  };
 
-  renderRestaurants(){
+  renderRestaurants() {
     if (!this.state.priceRangeRestaurants) {
       return <h2>Select a Price Range!</h2>;
     }
-    return this.state.priceRangeRestaurants.map( r => {
+    return this.state.priceRangeRestaurants.map(r => {
       return (
-        <CategoryCard key={r._id} name={r.name} category={this.state.priceRange}/>
-      )
+        <CategoryCard
+          key={r._id}
+          name={r.name}
+          description={r.description}
+          images={r.images}
+          category={this.state.priceRange}
+        />
+      );
     });
   }
 
@@ -47,31 +56,39 @@ class Prices extends React.Component {
       <div
         id="reservationgrid"
         className="ui sixteen wide column two column relaxed stackable grid middle aligned content container">
-          <div className="four wide column">
-            <div className="ui header">Restaurants prices</div>
-            <div className="ui secondary vertical pointing menu">
-              <button onClick={() => this.fetchRestaurants("10-20")} className="item">
-                10-20 (Dollars)
-              </button>
-              <button onClick={() => this.fetchRestaurants("20-30")} className="item">
-                20-30 (Dollars)
-              </button>
-              <button onClick={() => this.fetchRestaurants("30-40")} className="item">
-                30-40 (Dollars)
-              </button>
-              <button onClick={() => this.fetchRestaurants("40-50")} className="item">
-                40-50 (Dollars)
-              </button>
-              <button onClick={() => this.fetchRestaurants("50-60")} className="item">
-                50-60 (Dollars)
-              </button>
-            </div>
+        <div className="four wide column">
+          <div className="ui header">Restaurants prices</div>
+          <div className="ui secondary vertical pointing menu">
+            <button
+              onClick={() => this.fetchRestaurants("10-20")}
+              className="item">
+              10-20 (Dollars)
+            </button>
+            <button
+              onClick={() => this.fetchRestaurants("20-30")}
+              className="item">
+              20-30 (Dollars)
+            </button>
+            <button
+              onClick={() => this.fetchRestaurants("30-40")}
+              className="item">
+              30-40 (Dollars)
+            </button>
+            <button
+              onClick={() => this.fetchRestaurants("40-50")}
+              className="item">
+              40-50 (Dollars)
+            </button>
+            <button
+              onClick={() => this.fetchRestaurants("50-60")}
+              className="item">
+              50-60 (Dollars)
+            </button>
           </div>
-          <div className="eight wide column internally celled grid">
-            <div>
-              {this.renderRestaurants()}
-            </div>
-          </div>
+        </div>
+        <div className="eight wide column internally celled grid">
+          <div>{this.renderRestaurants()}</div>
+        </div>
       </div>
     );
   }
