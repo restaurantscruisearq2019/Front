854ccd541ce2d15cf2e1b6c1f86f34d56bcf725d
Arka Ariza <arjarizami@unal.edu.co>
2019-06-16 23:26:17 -0500

    Fixed Menus

--- restaurants/src/components/ListMenus.js
+++ restaurants/src/components/ListMenus.js
@@ -1,16 +1,15 @@
 import React from "react";
 import gql from "graphql-tag";
 import apollo from "../api/apollo";
-import CategoryCard from './CategoryCard';
+import CategoryCard from "./CategoryCard";
 
 class ListMenus extends React.Component {
-
   state = {
-    category: null, 
+    category: null,
     categoryMenuRestaurants: null
-  }
+  };
 
-  fetchRestaurants = ( a ) => {
+  fetchRestaurants = a => {
     apollo
       .query({
         query: gql`
@@ -18,27 +17,37 @@ class ListMenus extends React.Component {
           getRestaurantsByMenu(mType: "${a}"){
             name
             _id
+            description
+            images
           }
         }        
         `
       })
       .then(data => {
-        this.setState({ categoryMenuRestaurants: data.data.getRestaurantsByMenu });
+        this.setState({
+          categoryMenuRestaurants: data.data.getRestaurantsByMenu
+        });
         this.setState({ category: a });
       })
       .catch(err => {
         console.log(err);
       });
-  }
+  };
 
-  renderRestaurants(){
+  renderRestaurants() {
     if (!this.state.categoryMenuRestaurants) {
       return <h2>Select a Type of Menu!</h2>;
     }
-    return this.state.categoryMenuRestaurants.map( r => {
+    return this.state.categoryMenuRestaurants.map(r => {
       return (
-        <CategoryCard key={r._id} name={r.name} category={this.state.category}/>
-      )
+        <CategoryCard
+          key={r._id}
+          name={r.name}
+          description={r.description}
+          images={r.images}
+          category={this.state.category}
+        />
+      );
     });
   }
 
@@ -49,30 +58,40 @@ class ListMenus extends React.Component {
         className="ui sixteen wide column two column relaxed stackable grid middle aligned content container">
         <div className="five wide column">
           <div className="ui secondary vertical menu">
-            <button onClick={() => this.fetchRestaurants("Burguers")} className="active item">
+            <button
+              onClick={() => this.fetchRestaurants("Burguers")}
+              className="active item">
               Burguers
             </button>
-            <button onClick={() => this.fetchRestaurants("Vegie")} className="active item">
+            <button
+              onClick={() => this.fetchRestaurants("Vegie")}
+              className="active item">
               Vegie
             </button>
-            <button onClick={() => this.fetchRestaurants("Meats")} className="active item">
+            <button
+              onClick={() => this.fetchRestaurants("Meats")}
+              className="active item">
               Meats
             </button>
-            <button onClick={() => this.fetchRestaurants("Italian")} className="active item">
+            <button
+              onClick={() => this.fetchRestaurants("Italian")}
+              className="active item">
               Italian
             </button>
-            <button onClick={() => this.fetchRestaurants("Pizza")} className="active item">
+            <button
+              onClick={() => this.fetchRestaurants("Pizza")}
+              className="active item">
               Pizza
             </button>
-            <button onClick={() => this.fetchRestaurants("Sushi")} className="active item">
+            <button
+              onClick={() => this.fetchRestaurants("Sushi")}
+              className="active item">
               Sushi
             </button>
           </div>
         </div>
         <div className="eight wide column internally celled grid">
-          <div>
-            {this.renderRestaurants()}
-          </div>
+          <div>{this.renderRestaurants()}</div>
         </div>
       </div>
     );
